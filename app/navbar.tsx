e096dad39485d1e385cc8274da48860fd88fb4f3
carlosveloso <carlosvelosomct@gmail.com>
2025-04-18 08:04:19 +0200

    Improve landing page navbar style

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -56,7 +56,8 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-100 text-gray-800 p-4 flex justify-between items-center shadow-md relative">
       {/* Logo or App Name */}
-      <div className="lg:w-60 text-lg font-bold">
+      {/* <div className="lg:w-210 text-lg font-bold"> */}
+      <div className="text-lg font-bold">
         <Link href="/" className="text-green-600 hover:underline">
           Finance Tracker
         </Link>
@@ -76,55 +77,56 @@ export default function Navbar() {
       <div
         className={`${
           isMenuOpen ? 'block' : 'hidden'
-        } absolute top-full left-0 w-full bg-white shadow-md lg:static lg:flex lg:items-center lg:gap-4 lg:w-auto lg:rounded-md lg:mx-auto z-50`}
+        // } absolute top-full left-0 w-full bg-white shadow-md lg:static lg:flex lg:items-center lg:gap-4 lg:w-auto lg:rounded-md z-50 border border-red-500`}
+        } absolute top-full left-0 w-full bg-white lg:bg-transparent lg:static lg:flex lg:items-center lg:gap-4 lg:w-auto z-50`}
       >
         {/* Navigation Links */}
-        <div className="flex flex-col gap-y-2 px-4 py-2 lg:flex-row lg:gap-4 lg:p-0">
+        <div className="flex flex-col gap-y-2 px-4 py-2 lg:flex-row lg:gap-4 lg:p-0 lg:rounded-md lg:border-t lg:border-gray-200 lg:border-b lg:border-gray-200">
           <Link
             href="/recurrent"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:underline hover:text-green-600 transition-colors duration-200"
+            className="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-green-600 transition-colors duration-200"
             onClick={handleLinkClick} // Close menu on click
           >
             Recurrent
           </Link>
           <Link
             href="/sjprio"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:underline hover:text-green-600 transition-colors duration-200"
+            className="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-green-600 transition-colors duration-200"
             onClick={handleLinkClick} // Close menu on click
           >
             SJ Prio
           </Link>
           <Link
             href="/amex"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:underline hover:text-green-600 transition-colors duration-200"
+            className="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-green-600 transition-colors duration-200"
             onClick={handleLinkClick} // Close menu on click
           >
             Amex
           </Link>
           <Link
             href="/handelsbanken"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:underline hover:text-green-600 transition-colors duration-200"
+            className="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-green-600 transition-colors duration-200"
             onClick={handleLinkClick} // Close menu on click
           >
             Handelsbanken
           </Link>
           <Link
             href="/global"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:underline hover:text-green-600 transition-colors duration-200"
+            className="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-green-600 transition-colors duration-200"
             onClick={handleLinkClick} // Close menu on click
           >
             Transactions
           </Link>
           <Link
             href="/about"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:underline hover:text-green-600 transition-colors duration-200"
+            className="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-green-600 transition-colors duration-200"
             onClick={handleLinkClick} // Close menu on click
           >
             About
           </Link>
           <Link
             href="/contact"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:underline hover:text-green-600 transition-colors duration-200"
+            className="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-green-600 transition-colors duration-200"
             onClick={handleLinkClick} // Close menu on click
           >
             Contact
@@ -132,15 +134,17 @@ export default function Navbar() {
         </div>
 
         {/* User Authentication */}
-        <div className="lg:w-60 flex flex-col gap-y-2 px-4 py-2 border-t border-gray-200 lg:border-none lg:flex-row lg:gap-4 lg:p-0 text-right">
+        {/* <div className="lg:w-60 flex flex-col gap-y-2 px-4 py-2 border-t border-gray-200 lg:border-none lg:flex-row lg:gap-4 lg:p-0 text-right"> */}
+        {/* <div className="lg:w-210 flex flex-col gap-y-2 px-4 py-2 border-t border-gray-200 lg:border-none lg:flex-row lg:gap-4 lg:p-0"> */}
+        <div className="flex flex-col gap-y-2 px-4 py-2 border-t border-gray-200 lg:border-none lg:flex-row lg:gap-4 lg:p-0">
           {user ? (
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="text-gray-800">
+                <Button variant="ghost" className="text-gray-800 lg:ml-auto">
                   {user.email}
                 </Button>
               </DropdownMenuTrigger>
-              <DropdownMenuContent align="end" className="bg-gray-100 text-gray-800 shadow-md rounded-md">
+              <DropdownMenuContent align="end" className="bg-gray-100 text-gray-800 shadow-md rounded-md lg:w-55">
                 <DropdownMenuItem
                   className="cursor-pointer hover:bg-gray-200 hover:underline hover:text-green-600 transition-colors duration-200"
                 >
@@ -158,7 +162,7 @@ export default function Navbar() {
             <Button
               onClick={handleLogin}
               variant="ghost"
-              className="w-full text-gray-800 border border-gray-300 px-4 py-2 rounded-md hover:bg-gray-100 hover:underline hover:text-green-600 transition-colors duration-200"
+              className="text-gray-800 border border-gray-300 px-4 py-2 rounded-md hover:bg-gray-100 hover:underline hover:text-green-600 transition-colors duration-200 ml-auto"
             >
               Log In
             </Button>

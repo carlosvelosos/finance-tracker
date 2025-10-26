6efe24e0163f6c8f93d426981dcce8693ae7da0f
Carlos Veloso <carlosvelosomct@gmail.com>
2025-10-26 12:52:42 +0100

    Fix email page error

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   // Remove 'output: export' for Vercel deployment
   trailingSlash: true, // Optional: Ensures proper routing for static files
   // Remove basePath unless deploying under a subpath
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
